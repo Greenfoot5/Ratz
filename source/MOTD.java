5266344c9874e9865700cd9ad0f04d59cd1c44a5
Greenfoot5 <greenfoot89@gmail.com>
2021-11-12 16:45:29 +0000

    Created the post solution to MOTD.java

--- source/MOTD.java
+++ source/MOTD.java
@@ -2,17 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class MOTD {
     public static void main(String[] args) throws IOException {
-        System.out.println(GetMOTD());
+        System.out.println(getPuzzle());
     }
 
-    public static String GetMOTD() throws IOException {
+    /**
+     * Uses a HTTP GET request to obtain the puzzle
+     * @return The puzzle to solve
+     * @throws IOException There was an issue with sending the request
+     */
+    public static String getPuzzle() throws IOException {
+        // Url to send it to
         String urlString = "http://cswebcat.swansea.ac.uk/puzzle";
         URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+        InputStream is = conn.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        return reader.readLine();
+    }
+
+    public static String solvePuzzle(String puzzleInput) {
+        return puzzleInput;
+    }
+
+    public static String postSolution(String solution) throws IOException {
+        String solutionUrl = "http://cswebcat.swansea.ac.uk/message?solution=" + solution;
+        URL url = new URL(solutionUrl);
         URLConnection conn = url.openConnection();
         InputStream is = conn.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
