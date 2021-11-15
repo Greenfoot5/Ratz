ed1480b3bb2ad00de6c797248446f5cfc661a1f3
translibrius <daumantas.balakauskas@gmail.com>
2021-11-15 22:47:14 +0000

    Added Poison class.

---
+++ source/Poison.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Poison extends Power{
+
+    //This class kills all rats that are passed to it.
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static void killRats(ArrayList<Rat> rats) {
+        for (int i : rats){
+            rats.get(i).die;
+        }
+    }
+
+    @Override
+    void activate(ArrayList<Rat> rats) {
+        killRats(rats);
+    }
+}
