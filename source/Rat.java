5962e44582376541bc7245e622e8fc4fafe0f852
Jems <jamesgmcw@gmail.com>
2021-11-17 18:14:22 +0000

    fixed rat constructor.

--- source/Rat.java
+++ source/Rat.java
@@ -4,27 +4,30 @@
  */
 
 public abstract class Rat extends Object {
-
     private int speed;
     private int direction; // TODO: swap this to an enum
     private int gasTimer;
     private int xPos;
     private int yPos;
 
-
     /**
      * Rat constructor.
      *
      * @param isInteractive can players place powers on.
-     * @param isPassable can rats walk through.
-     * @param speed how fast the rat moves.
-     * @param direction the direction the rat is facing.
-     * @param gasTimer how long the rat has spent inside poison gas.
-     * @param xPos where the rat is on the x axis.
-     * @param yPos where the rat is on the y axis.
+     * @param isPassable    can rats walk through.
+     * @param speed         how fast the rat moves.
+     * @param direction     the direction the rat is facing.
+     * @param gasTimer      how long the rat has spent inside poison gas.
+     * @param xPos          where the rat is on the x axis.
+     * @param yPos          where the rat is on the y axis.
      */
     Rat(boolean isInteractive, boolean isPassable, int speed, int direction, int gasTimer, int xPos, int yPos) {
         super(isInteractive, isPassable);
+        this.speed = speed;
+        this.direction = direction;
+        this.gasTimer = gasTimer;
+        this.xPos = xPos;
+        this.yPos = yPos;
     }
 
 
