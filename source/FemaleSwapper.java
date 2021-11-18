af1a33407d7e66384a2b15f9bffee7ac714c42c0
translibrius <daumantas.balakauskas@gmail.com>
2021-11-18 18:21:59 +0000

    Added gender swapper classes.

---
+++ source/FemaleSwapper.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class FemaleSwapper extends Power{
+
+    FemaleSwapper(boolean isInteractive, boolean isPassable) {
+        super(isInteractive, isPassable);
+    }
+
+    @Override
+    void activate(ArrayList<Rat> rats) {
+        /*
+        if(rats.get(0).getGender == "male") {
+
+        }
+
+        Need to copy the rat that stepped on, kill it, spawn a copy of the old one but with opposite gender.
+        */
+    }
+    }
+    
+}
---
+++ source/MaleSwapper.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class MaleSwapper extends Power{
+
+    MaleSwapper(boolean isInteractive, boolean isPassable) {
+        super(isInteractive, isPassable);
+    }
+
+    @Override
+    void activate(ArrayList<Rat> rats) {
+        /*
+        if(rats.get(0).getGender == "female") {
+
+        }
+
+        Need to copy the rat that stepped on, kill it, spawn a copy of the old one but with opposite gender.
+        */
+    }
+    
+}
