8890078622b1cd0f6d7fda0ce2f893ec0777f13a
translibrius <daumantas.balakauskas@gmail.com>
2021-11-18 18:09:33 +0000

    added Gas basic stuff

---
+++ source/Gas.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Gas extends Power{
+
+    Gas(boolean isInteractive, boolean isPassable) {
+        super(isInteractive, isPassable);
+    }
+
+    private static void spreadTimer() {
+        long startTime = System.currentTimeMillis();
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        long elapsedSeconds = elapsedTime / 1000;
+        //To-Do after 5s ?
+    }
+
+    private static void dissapationTimer() {
+        long startTime = System.currentTimeMillis();
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        long elapsedSeconds = elapsedTime / 1000;
+        //To-Do after 5s ?
+    }
+
+    @Override
+    void activate(ArrayList<Rat> rats) {
+        for (Rat r : rats) {
+            r.incGasTimer();
+        }
+        
+    }
+    
+}
