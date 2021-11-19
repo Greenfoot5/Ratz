d37d41a165bf3e36a9263e4e816253bd9dae32c7
Greenfoot5 <greenfoot89@gmail.com>
2021-11-19 15:01:41 +0000

    Method comments

--- source/MainMenuManager.java
+++ source/MainMenuManager.java
@@ -3,7 +3,7 @@ import javafx.stage.Stage;
 
 public class MainMenuManager extends Application {
     // Profiles
-    private String selectedProfile;
+    private String selectedProfile = null;
 
     // The dimensions of the window
     private static final int WINDOW_WIDTH = 800;
@@ -13,10 +13,19 @@ public class MainMenuManager extends Application {
     private static final int CANVAS_WIDTH = 600;
     private static final int CANVAS_HEIGHT = 400;
 
+    /**
+     * Launches the application
+     * @param args Don't do anything atm
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Called when we begin the application
+     * @param primaryStage I forgot
+     * @throws Exception /shrug
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         // TODO - Implement LevelManager stuff
@@ -26,15 +35,25 @@ public class MainMenuManager extends Application {
         //loader.setController(levelController);
     }
 
+    /**
+     * Displays the level select screen
+     */
     private void displayLevels() {}
 
+    /**
+     * Allows the user to login to a profile
+     * @param username The username to login to
+     */
     public void login(String username) {
         ProfileFileReader reader = new ProfileFileReader();
         // TODO - Check if the profile exists
         reader.createNewProfile(username);
         selectedProfile = username;
     }
-    
+
+    /**
+     * Logs the user out of an account.
+     */
     public void logout() {
         selectedProfile = null;
     }
