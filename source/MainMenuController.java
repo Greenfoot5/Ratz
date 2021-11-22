9983dcf7004c22d140d1cb1d760f8f5b1a801953
Greenfoot5 <greenfoot89@gmail.com>
2021-11-22 15:26:09 +0000

    Main Menu now has a layout.

--- source/MainMenuController.java
+++ source/MainMenuController.java
@@ -1,13 +1,17 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  * The main class to begin, and the one that handles the main menu
  */
 public class MainMenuController extends Application {
-    // Profiles
-    private String selectedProfile = null;
-
     // The dimensions of the window
     private static final int WINDOW_WIDTH = 800;
     private static final int WINDOW_HEIGHT = 500;
@@ -15,6 +19,9 @@ public class MainMenuController extends Application {
     // The dimensions of the canvas
     private static final int CANVAS_WIDTH = 600;
     private static final int CANVAS_HEIGHT = 400;
+
+    // Profiles
+    private String selectedProfile = null;
 
     /**
      * Launches the application
@@ -27,15 +34,27 @@ public class MainMenuController extends Application {
     /**
      * Called when we begin the application
      * @param primaryStage I forgot
-     * @throws Exception /shrug
      */
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        // TODO - Implement LevelManager stuff
-        //loader = new FXMLLoader(getClass().getResource("level.fxml"));
-        //LevelController levelController = new LevelController(--level file reader instance goes here--);
-        //
-        //loader.setController(levelController);
+    public void start(Stage primaryStage) {
+        // Create a new pane to hold our GUI
+        VBox root = new VBox();
+        root.setAlignment(javafx.geometry.Pos.CENTER);
+
+        // Create a few GUI elements
+        Label title = new Label("RATZ");
+        Button playButton = new Button("Play!");
+        Label outputLabel = new Label("I am a label");
+        root.getChildren().addAll(title, playButton, outputLabel);
+        // Handle a button event
+        playButton.setOnAction(event -> outputLabel.setText("I was Clicked"));
+
+        // Create a scene based on the pane.
+        Scene scene = new Scene(root, 400, 400);
+
+        // Show the scene
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     /**
