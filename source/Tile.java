9a35a318d9c2c46c4d5d27f767ea17a30afc8b99
Nhysalotep <thunderclaw98@gmail.com>
2021-11-22 12:37:09 +0000

    Issues with lists

--- source/Tile.java
+++ source/Tile.java
@@ -1,6 +1,37 @@
-public class Tile extends Object {
+/**
+ * Abstract class that defines what a tile even is anyway
+ * @author Alexander Douglas Lloyd-Ward
+ */
+
+public class Tile extends GameObject {
     private boolean isPassable;
     private boolean canRatMove;
-    private Object activePowers[];
-    private Object occupantRats [];
+    private Arraylist<Power> activePowers = new ArrayList<Power>();
+    private ArrayList<LivingRat> occupantRats = new ArrayList<LivingRat>();
+
+    public Tile(boolean isPassable, boolean canRatMove, Arraylist<Power> activePowers, ArrayList<LivingRat> occupantRats) {
+        this.isPassable = isPassable;
+        this.canRatMove = canRatMove;
+        this.activePowers = activePowers;
+        this.occupantRats = occupantRats;
+    }
+
+    public void setCanRatMove(boolean canRatMove) {
+        this.canRatMove = canRatMove;
+    }
+    public Tile(boolean canRatMove) {
+        this.canRatMove = canRatMove;
+    }
+    public boolean isCanRatMove() {
+        return canRatMove;
+    }
+
+    public void setPassable(boolean passable) {
+        isPassable = passable;
+    }
+    public boolean isPassable() {
+        return isPassable;
+    }
+
 }
+
--- source/Tunnel.java
+++ source/Tunnel.java
@@ -1,2 +1,3 @@
 public class Tunnel extends Tile {
+    @
 }
