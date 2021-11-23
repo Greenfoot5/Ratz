39e0e26842505ab79d491f903bd8c5fc5e1b6493
Your Name <jamesgmcw@gmail.com>
2021-11-23 23:13:39 +0000

    levelFileReader now gets height, width, and tiles. Also added getTile which retrieves a specific tile.
    Signed-off-by: Your Name <jamesgmcw@gmail.com>

--- source/LevelFileReader.java
+++ source/LevelFileReader.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  * A class which reads levels from files, and saves levels to files.
@@ -7,7 +11,7 @@ import java.io.File;
 public class LevelFileReader {
     private static int height;
     private static int width;
-    private static String[][] tiles;
+    private static String[] tiles;
     private static String[] ratSpawns;
     private static String[] powers;
     private static int maxRats;
@@ -29,10 +33,19 @@ public class LevelFileReader {
      * G for grass, P for path, and T for tunnel.
      * @return tiles
      */
-    public static String[][] getTiles() {
+    public static String[] getTiles() {
         return tiles;
     }
 
+    /**
+     * Returns a tile at the specified location. G for grass, P for path, T for tunnel.
+     * @param x X coordinate of the tile.
+     * @param y Y coordinate of the tile.
+     * @return A char representing the type of tile in the given location.
+     */
+    public static char getTile(int x, int y) {
+        return tiles[y].charAt(x);
+    }
 
     public static String[] getRatSpawns() {
         return ratSpawns;
@@ -60,16 +73,39 @@ public class LevelFileReader {
         return dropRates;
     }
 
-    public void saveLevel(int size, String[][] tiles, String[] ratSpawns, String[] powers,
+    public void saveLevel(int size, String[] tiles, String[] ratSpawns, String[] powers,
                           int maxRats, int parTime, int[] dropRates){
         // save shit to a file
     }
 
-    public void saveLevelState(int size, String[][] tiles, String[] ratSpawns, String[] powers) {
+    public void saveLevelState(int size, String[] tiles, String[] ratSpawns, String[] powers) {
         // save shit to a file.
     }
 
-    public void loadLevelFile(File file) {
+    public static void loadLevelFile(String filename) throws FileNotFoundException {
+        File levelData = new File(filename);
+
+        Scanner reader = new Scanner(levelData);
+
+        // get level width and height
+        if (reader.hasNextLine()) {
+            String[] levelSize = reader.nextLine().split(",");
+            width = Integer.parseInt(levelSize[0]);
+            height = Integer.parseInt(levelSize[1]);
+        }
+
+
+        // get tile data
+        String currentTiles = "";
+        for (int i = 0; i < height; i++) {
+            if (reader.hasNext()) {
+                currentTiles = currentTiles.concat(reader.nextLine());
+            }
+        }
+
+        // this ugly regex splits currentTiles based on the level's width
+        tiles = currentTiles.split("(?<=\\G.{" + width + "})");
+        reader.close();
 
     }
 
