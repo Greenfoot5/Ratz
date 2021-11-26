dbef6a3710e50a966670339b1c38c4f0b55cfdfb
translibrius <daumantas.balakauskas@gmail.com>
2021-11-26 06:43:38 +0000

    Partial implementation of FemaleSwapper -/ Full implementation of Bomb.

--- source/Bomb.java
+++ source/Bomb.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
 
 /**
- * A class that kills rats in all directions until it hits a grass Tile after
- * 5 ticks.
+ * A class that kills all rats standing on the tile at the time of explosion -
+ * (5 game ticks after it's been placed).
  * @author Daumantas Balakauskas
  */
 
 public class Bomb extends Power {
+
+    private int ticksActive = 0; //Tick counter since creation of this class.
 
     /**
      * Bomb constructor
@@ -20,22 +22,11 @@ public class Bomb extends Power {
     }
 
     /**
-     * Abstract method for certain powers that need to activate after a
-     * certain amount of time.
-     */
-
-    @Override
-    void onTick(ArrayList<Rat> rats, Tile currentTile) {
-
-    }
-
-    /**
      * Abstract method to let any power know it's time to do something.
-     * For Bomb - kill all rats in the current tile and adds another bomb
-     * following Tiles in a direction that aren't grass and instantly
-     * activates itself.
+     * For Bomb - kill all rats standing on it's Tile at the time of explosion.
      *
      * @param rats used to interact with all rats that stepped on the power.
+     * @param currentTile used to remove this Power from Tile.
      */
 
     @Override
@@ -43,8 +34,23 @@ public class Bomb extends Power {
         for (Rat r : rats) {
             r.die();
         }
-
-
+        currentTile.removeActivePower(this);
     }
-    
+
+    /**
+     * Abstract method for certain powers that need to activate after a
+     * certain amount of time.
+     * @param currentTile used for calling removeActivePower(this).
+     * @param rats used for updating the rat arraylist every game tick.
+     */
+
+    @Override
+    void onTick(ArrayList<Rat> rats, Tile currentTile) {
+        ticksActive = ticksActive + 1;
+        if (ticksActive < 5) {
+            activate(rats, currentTile);
+        } else {
+            currentTile.removeActivePower(this);
+        }
+    }
 }
--- source/FemaleSwapper.java
+++ source/FemaleSwapper.java
@@ -1,25 +1,49 @@
 import java.util.ArrayList;
 
+/**
+ * A class that swaps male rat to female if it steps on this Power.
+ * @author Daumantas Balakauskas
+ */
+
 public class FemaleSwapper extends Power {
+
+    /** FemaleSwapper constructor
+     *
+     * @param isInteractive can player place power on.
+     * @param isPassable can rats walk through.
+     */
 
     FemaleSwapper(boolean isInteractive, boolean isPassable) {
         super(isInteractive, isPassable);
     }
 
+
+
     @Override
     void activate(ArrayList<Rat> rats, Tile currentTile) {
-        /*
-        if(rats.get(0).getGender == "male") {
+        for (Rat r : rats) {
+            if(r instanceof AdultMale) {
+                /* Will not work until AdultMale&AdultFemale getters are set up.
 
+                AdultFemale copyRat = new AdultFemale(true,false,
+                        r.getSpeed(), r.getDirection(), r.getGasTimer(),
+                        r.getX(), r.getY(), r.getFertile());
+
+                currentTile.addOccupantRat(copyRat);
+                currentTile.removeOccupantRat(r);
+                */
+                currentTile.removeActivePower(this);
+            }
         }
-
-        Need to copy the rat that stepped on, kill it, spawn a copy of the old one but with opposite gender.
-        */
     }
 
     /**
      * Abstract method for certain powers that need to activate after a
      * certain amount of time.
+     * @param currentTile used for calling removeActivePower(this).
+     * @param rats used for updating the rat arraylist every game tick.
+     *
+     * onTick not used for this Power.
      */
 
     @Override
