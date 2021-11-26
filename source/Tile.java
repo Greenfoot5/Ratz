014452764c8765f1c96dc5d5dda75043eb8bfffa
Nhysalotep <thunderclaw98@gmail.com>
2021-11-26 02:32:54 +0000

    fixup! Added a little more javadoc. Changed the code for adders. Added base for removers

--- source/Tile.java
+++ source/Tile.java
@@ -3,42 +3,42 @@ import java.util.ArrayList;//
 /** //
  * Abstract class that defines what a tile even is anyway//
  * @author Alexander Douglas Lloyd-Ward//
- *///
+ */
 
-public class Tile extends GameObject {//
+public class Tile extends GameObject {
     //private boolean canRatMove;
-    private ArrayList<Power> activePowers;//
-    private ArrayList<LivingRat> occupantRats;//
-//
-    /** //
-     * Tile constructor.//
-     * //
-     * @param activePowers    What powers are active on this tile.//
-     * @param occupantRats    What rats are present on this tile.//
-     *///
-    public Tile(boolean isPassable,/* boolean canRatMove,*/ ArrayList<Power> activePowers, ArrayList<LivingRat> occupantRats) {//
-        super(false, isPassable);//
-        //this.canRatMove = canRatMove;
-        this.activePowers = activePowers;//
-        this.occupantRats = occupantRats;//
-    }//
-//
-  //  //========================Getters and setters========================
+    private ArrayList<Power> activePowers;
+    private ArrayList<LivingRat> occupantRats;
 
-    public ArrayList<Power> getActivePowers() {//
-        return activePowers;//
-    }//
-//
-    public void setActivePowers(ArrayList<Power> activePowers) {//
-        /*this.activePowers.add(new Power());*///
-    }//
-//
-    public ArrayList<LivingRat> getOccupantRats() {//
-        return occupantRats;//
-    }//
-//
-    public void setOccupantRats(ArrayList<LivingRat> occupantRats) {//
-        this.occupantRats = occupantRats;//
-    }//
-}//
+    /**
+     * Tile constructor.
+     *
+     * @param activePowers    What powers are active on this tile.
+     * @param occupantRats    What rats are present on this tile.
+     */
+    public Tile(boolean isPassable,/* boolean canRatMove,*/ ArrayList<Power> activePowers, ArrayList<LivingRat> occupantRats) {
+        super(false, isPassable);
+        //this.canRatMove = canRatMove;
+        this.activePowers = activePowers;
+        this.occupantRats = occupantRats;
+    }
+
+    //========================Getters and setters========================
+
+    public ArrayList<Power> getActivePowers() {
+        return activePowers;
+    }
+
+    public void setActivePowers(ArrayList<Power> activePowers) {
+        /*this.activePowers.add(new Power());*/
+    }
+
+    public ArrayList<LivingRat> getOccupantRats() {
+        return occupantRats;
+    }
+
+    public void setOccupantRats(ArrayList<LivingRat> occupantRats) {
+        this.occupantRats = occupantRats;
+    }
+}
 //comment
--- source/Tunnel.java
+++ source/Tunnel.java
@@ -1,15 +1,14 @@
-import java.util.ArrayList;//
-/** //
- * Class that defines a Tunnel.//
- * @author Alexander Douglas Lloyd-Ward//
- *///
-public class Tunnel extends Tile {//
- /** //
- * Tunnel constructor.//
- * Doesn't really do anything that the Tile doesn't do.//
- *///
+import java.util.ArrayList;
+/**
+ * Class that defines a Tunnel.
+ * @author Alexander Douglas Lloyd-Ward
+ */
+public class Tunnel extends Tile {
+ /**
+ * Tunnel constructor.
+ * Doesn't really do anything that the Tile doesn't do.
+ */
     public Tunnel() {
         super(true, new ArrayList<>(), new ArrayList<>());
-    }//
-}//
-//comment
+    }
+}
