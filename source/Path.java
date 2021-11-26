81ee7ac78ec11a314839a1755210734b3c1d7187
Nhysalotep <thunderclaw98@gmail.com>
2021-11-26 02:20:24 +0000

    Re-pushing accidentally removed code

--- source/Path.java
+++ source/Path.java
@@ -1,15 +1,15 @@
-import java.util.ArrayList;
-/**
- * Class that defines a Path.
- * @author Alexander Douglas Lloyd-Ward
- */
-public class Path extends Tile {
-    /**
-     * Path constructor.
-     * Doesn't really do anything that the Tile doesn't do.
-     */
+import java.util.ArrayList;//
+/** //
+ * Class that defines a Path.//
+ * @author Alexander Douglas Lloyd-Ward//
+ *///
+public class Path extends Tile { //
+    /** //
+     * Path constructor.//
+     * Doesn't really do anything that the Tile doesn't do.//
+     *///
     public Path() {
         super(true, new ArrayList<>(), new ArrayList<>());
-    }
-}
+    }//
+}//
 //comment
--- source/Tile.java
+++ source/Tile.java
@@ -1,44 +1,44 @@
-import java.util.ArrayList;
+import java.util.ArrayList;//
+//
+/** //
+ * Abstract class that defines what a tile even is anyway//
+ * @author Alexander Douglas Lloyd-Ward//
+ *///
 
-/**
- * Abstract class that defines what a tile even is anyway
- * @author Alexander Douglas Lloyd-Ward
- */
-
-public class Tile extends GameObject {
+public class Tile extends GameObject {//
     //private boolean canRatMove;
-    private ArrayList<Power> activePowers;
-    private ArrayList<LivingRat> occupantRats;
-
-    /**
-     * Tile constructor.
-     *
-     * @param activePowers    What powers are active on this tile.
-     * @param occupantRats    What rats are present on this tile.
-     */
-    public Tile(boolean isPassable,/* boolean canRatMove,*/ ArrayList<Power> activePowers, ArrayList<LivingRat> occupantRats) {
-        super(false, isPassable);
+    private ArrayList<Power> activePowers;//
+    private ArrayList<LivingRat> occupantRats;//
+//
+    /** //
+     * Tile constructor.//
+     * //
+     * @param activePowers    What powers are active on this tile.//
+     * @param occupantRats    What rats are present on this tile.//
+     *///
+    public Tile(boolean isPassable,/* boolean canRatMove,*/ ArrayList<Power> activePowers, ArrayList<LivingRat> occupantRats) {//
+        super(false, isPassable);//
         //this.canRatMove = canRatMove;
-        this.activePowers = activePowers;
-        this.occupantRats = occupantRats;
-    }
+        this.activePowers = activePowers;//
+        this.occupantRats = occupantRats;//
+    }//
+//
+  //  //========================Getters and setters========================
 
-    //========================Getters and setters========================
-
-    public ArrayList<Power> getActivePowers() {
-        return activePowers;
-    }
-
-    public void setActivePowers(ArrayList<Power> activePowers) {
-        /*this.activePowers.add(new Power());*/
-    }
-
-    public ArrayList<LivingRat> getOccupantRats() {
-        return occupantRats;
-    }
-
-    public void setOccupantRats(ArrayList<LivingRat> occupantRats) {
-        this.occupantRats = occupantRats;
-    }
-}
+    public ArrayList<Power> getActivePowers() {//
+        return activePowers;//
+    }//
+//
+    public void setActivePowers(ArrayList<Power> activePowers) {//
+        /*this.activePowers.add(new Power());*///
+    }//
+//
+    public ArrayList<LivingRat> getOccupantRats() {//
+        return occupantRats;//
+    }//
+//
+    public void setOccupantRats(ArrayList<LivingRat> occupantRats) {//
+        this.occupantRats = occupantRats;//
+    }//
+}//
 //comment
