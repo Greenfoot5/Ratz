9a6e96e7b93be451782f0379f3c4d5240de13858
Vilija <vilija.pundyte@gmail.com>
2021-11-27 13:44:47 +0000

    Made proper Path rendering and added isInteractive cases

--- source/Path.java
+++ source/Path.java
@@ -1,3 +1,5 @@
+import javafx.scene.canvas.GraphicsContext;
+
 import java.util.ArrayList;
 
 /**
@@ -13,4 +15,59 @@ public class Path extends Tile {
     public Path() {
         super(true, new ArrayList<>(), new ArrayList<>());
     }
+
+    /**
+     * Draws path and all of the powers and rats on it
+     * @param x Horizontal position.
+     * @param y Vertical position.
+     * @param g Graphics context being drawn on.
+     */
+    @Override
+    public void draw(int x, int y, GraphicsContext g) {
+        int xDraw = GameObject.getWIDTH() * x;
+        int yDraw = GameObject.getWIDTH() * y;
+
+        g.drawImage(getImg(),xDraw,yDraw);
+
+        boolean drawGas = false;
+
+        for(int i = 0; i < getActivePowers().size() ; i++) {
+            for(Power p: getActivePowers()) {
+                if(p instanceof Gas){
+                    drawGas = true;
+                } else {
+                    p.draw(x,y,g);
+                }
+            }
+        }
+
+        for(int i = 0; i < getOccupantRats().size() ; i++) {
+            for(Rat r: getOccupantRats()) {
+                r.draw(x,y,g);
+            }
+        }
+
+        if (drawGas) {
+            (new Gas()).draw(x,y,g);
+        }
+
+    }
+
+    /**
+     * Returns whether players can place items on the tile.
+     * @return interactivity.
+     */
+    @Override
+    public boolean isInteractive() {
+        if(getOccupantRats().size() != 0) {
+            return false;
+        }
+        for (Power p: getActivePowers()) {
+            if(p instanceof StopSign) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
