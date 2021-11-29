219250d0a5f1c7a8cda2af00e05d0ae09d8f4341
Greenfoot5 <greenfoot89@gmail.com>
2021-11-29 12:20:19 +0000

    Completed the MOTD puzzle

--- source/MOTD.java
+++ source/MOTD.java
@@ -7,7 +7,9 @@ import java.net.URLConnection;
 
 public class MOTD {
     public static void main(String[] args) throws IOException {
-        System.out.println(getPuzzle());
+        String puzzle = getPuzzle();
+        puzzle = solvePuzzle(puzzle);
+        System.out.println(postSolution(puzzle));
     }
 
     /**
@@ -25,8 +27,52 @@ public class MOTD {
         return reader.readLine();
     }
 
+    /**
+     * Solves the puzzle according to the puzzle specification
+     * @param puzzleInput The input from the puzzle
+     * @return The solved puzzle
+     */
     public static String solvePuzzle(String puzzleInput) {
-        return puzzleInput;
+        // Covert the input into it's ascii values
+        char[] puzzleArray = puzzleInput.toCharArray();
+        int[] puzzleNumbers = new int[puzzleArray.length];
+        for (int i = 0; i < puzzleArray.length; i++) {
+            puzzleNumbers[i] = puzzleArray[i];
+        }
+
+        // Translate the characters one forward, two backward, etc.
+        int base = 'A';
+        for (int i = 0; i < puzzleNumbers.length; i++)
+        {
+            puzzleNumbers[i] -= base;
+            switch(i % 2)
+            {
+                // Translate forwards
+                case 1:
+                    puzzleNumbers[i] += (i + 1);
+                    while (puzzleNumbers[i] >= 26) {
+                        puzzleNumbers[i] -= 26;
+                    }
+                    break;
+                // Translate backwards
+                case 0:
+                    puzzleNumbers[i] -= (i + 1);
+                    while (puzzleNumbers[i] < 0) {
+                        puzzleNumbers[i] += 26;
+                    }
+                    break;
+            }
+            puzzleNumbers[i] += base;
+        }
+
+        // Convert back to characters
+        StringBuilder output = new StringBuilder();
+        for (int puzzleNumber : puzzleNumbers) {
+            output.append((char) puzzleNumber);
+        }
+        output.append("CS-230");
+
+        return Integer.toString(output.length()) + output;
     }
 
     public static String postSolution(String solution) throws IOException {
