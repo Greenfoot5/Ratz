c7323e1ef59ce786147dd2da5171246e9684e13a
Greenfoot5 <greenfoot89@gmail.com>
2021-12-06 02:06:52 +0000

    More file cleanups

--- source/DeathRat.java
+++ source/DeathRat.java
@@ -1,13 +1,16 @@
-import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Objects;
 
 /**
- * An class to model the death rat power-up.
+ * A class to model the death rat power-up.
  *
  * @author James McWilliams
+ * @version 1.0
  */
 
 public class DeathRat extends Rat {
+
+    private static final int MAX_KILL_COUNT = 5;
+
     private int ominousWaiting;
     private int killCounter;
 
@@ -17,8 +20,8 @@ public class DeathRat extends Rat {
      * @param speed       how fast the rat moves.
      * @param direction   the direction the rat is facing.
      * @param gasTimer    how long the rat has spent inside poison gas.
-     * @param xPos        where the rat is on the x axis.
-     * @param yPos        where the rat is on the y axis.
+     * @param xPos        where the rat is on the x-axis.
+     * @param yPos        where the rat is on the y-axis.
      * @param killCounter how many times the death rat has killed another rat
      */
     public DeathRat(int speed, Direction direction, int gasTimer, int xPos,
@@ -36,12 +39,18 @@ public class DeathRat extends Rat {
         if (killCounter >= 5) {
             die();
         }
-        if(ominousWaiting > 0) {
+
+        if (ominousWaiting > 0) {
             ominousWaiting--;
         }
+
         killRats();
     }
 
+    /**
+     * How long the rat has left till it moves
+     * @return how long the rat has left to wait
+     */
     public int getOminousWaiting() {
         return ominousWaiting;
     }
@@ -55,7 +64,8 @@ public class DeathRat extends Rat {
         // kill every rat on the tile unless your kill counter is 5 or greater
 
         int i = 0;
-        while (currentTile.getOccupantRats().size() > 1 && killCounter < 5) {
+        while (Objects.requireNonNull(currentTile).getOccupantRats().size() > 1
+                && killCounter < MAX_KILL_COUNT) {
             if(currentTile.getOccupantRats().get(i) != this) {
                 currentTile.getOccupantRats().get(i).die();
             } else {
