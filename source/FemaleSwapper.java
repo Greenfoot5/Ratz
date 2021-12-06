987b0da3a705b860afe317d089df334dc4d23d39
Greenfoot5 <greenfoot89@gmail.com>
2021-12-06 04:15:40 +0000

    Completed FemaleSwapper

--- source/FemaleSwapper.java
+++ source/FemaleSwapper.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
  * A class that swaps male rat to female if it steps on this Power.
  *
  * @author Daumantas Balakauskas
+ * @version 1.0
  */
 
 public class FemaleSwapper extends Power {
@@ -17,13 +18,14 @@ public class FemaleSwapper extends Power {
      * @param xPos x coordinate
      * @param yPos y coordinate
      */
-
     FemaleSwapper(int xPos, int yPos) {
         super(true, xPos, yPos);
     }
 
 
     /**
+     * Activates the power
+     *
      * @param rats        used to interact with all rats that stepped on the power.
      * @param currentTile used to access Tile of the Power.
      */
@@ -33,11 +35,12 @@ public class FemaleSwapper extends Power {
         SeaShantySimulator seaSim = new SeaShantySimulator();
         seaSim.playAudioClip(FEMALE_SWAP_SOUND_PATH, 0.1);
 
+        // Loop through the rats and attempt to change it's gender
         for (Rat r : rats) {
             if (r instanceof AdultMale) {
-                AdultFemale copyRat = new AdultFemale(r.getSpeed(), r.getDirection(), r.getGasTimer(),
-                        this.xPos, this.yPos, ((AdultMale) r).isFertile, 0
-                        , 0);
+                AdultFemale copyRat = new AdultFemale(r.getSpeed(),
+                        r.getDirection(), r.getGasTimer(), this.xPos,
+                        this.yPos, ((AdultMale) r).isFertile, 0, 0);
 
                 currentTile.removeActivePower(this);
                 currentTile.addOccupantRat(copyRat);
@@ -48,6 +51,7 @@ public class FemaleSwapper extends Power {
             } else {
                 currentTile.removeActivePower(this);
             }
+
             if (r instanceof ChildRat) {
                 ((ChildRat) r).setIsFemale(true);
             }
@@ -55,16 +59,12 @@ public class FemaleSwapper extends Power {
     }
 
     /**
-     * Abstract method for certain powers that need to activate after a
-     * certain amount of time.
+     * We don't do anything on tick
      *
      * @param currentTile used for calling removeActivePower(this).
      * @param rats        used for updating the rat arraylist every game tick.
      *                    onTick not used for this Power.
      */
-
     @Override
-    void onTick(ArrayList<Rat> rats, Tile currentTile) {
-
-    }
+    void onTick(ArrayList<Rat> rats, Tile currentTile) {}
 }
