8424b8e724e006b36130e38c87a0f78f45becc88
TomaszFiji <93390031+TomaszFiji@users.noreply.github.com>
2022-02-10 19:15:26 +0000

    Added profile selection

--- source/MenuController.java
+++ source/MenuController.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -12,15 +13,26 @@ public class MenuController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	private Integer selectedLevel = 1;
 
 	public void buttonCl() {
-		System.out.println("asdasd");
+		System.out.println("button clicked");
 	}
 
 	public void changeToLevelSelection(ActionEvent event) throws IOException {
-		System.out.println("asdasd2222");
+		System.out.println("move to level selection");
 
-		Parent root = FXMLLoader.load(getClass().getResource("levelsS.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("levelsSelection.fxml"));
+		stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void changeToProfileSelection(ActionEvent event) throws IOException {
+		System.out.println("move to profile selection");
+
+		Parent root = FXMLLoader.load(getClass().getResource("profileSelection.fxml"));
 		stage = (Stage)((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
@@ -28,7 +40,7 @@ public class MenuController {
 	}
 	
 	public void changeToMenu(ActionEvent event) throws IOException {
-		System.out.println("asdasd2222");
+		System.out.println("move to menu");
 
 		Parent root = FXMLLoader.load(getClass().getResource("menu2.fxml"));
 		stage = (Stage)((Node) event.getSource()).getScene().getWindow();
