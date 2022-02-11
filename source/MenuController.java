4a8b0365881547f9a4ab68e940042e86d2b9fa80
TomaszFiji <93390031+TomaszFiji@users.noreply.github.com>
2022-02-11 19:16:30 +0000

    removing and adding profiles added

--- source/MenuController.java
+++ source/MenuController.java
@@ -1,14 +1,20 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MenuController {
@@ -19,14 +25,50 @@ public class MenuController {
 	private Integer selectedLevel = 1;
 	private boolean profilesViewUpdated = false;
 
-	@FXML private VBox profileButtons;
-	
+	@FXML
+	private Button addProfilebutton;
+	@FXML
+	private VBox bestScoresLabel;
+	@FXML
+	private Label l1;
+	@FXML
+	private Label loggedProfileLabel;
+	@FXML
+	private TextField newProfileTextField;
+	@FXML
+	private VBox profileButtons;
+	@FXML
+	private Button removeProfileButton;
 
-    @FXML
-    public Label l1;
+	/**
+	 * Generates a popup alert
+	 *
+	 * @param message The message to display in the alert
+	 */
+	private void alert(String message) {
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle("Alert");
+		window.setWidth(200);
+		window.setHeight(200);
 
-	public void buttonCl() {
-		System.out.println("button clicked");
+		Label label = new Label(message);
+		Button button = new Button("Ok");
+		button.setPrefWidth(100);
+		button.setOnAction(event -> window.close());
+
+		VBox layout = new VBox(10);
+		layout.getChildren().addAll(label, button);
+		layout.setAlignment(Pos.CENTER);
+
+		Scene scene = new Scene(layout);
+		File f = new File("source/menu.css");
+		scene.getStylesheets().clear();
+		scene.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
+
+		window.setScene(scene);
+		window.setResizable(false);
+		window.showAndWait();
 	}
 
 	public void changeToLevelSelection(ActionEvent event) throws IOException {
@@ -49,6 +91,94 @@ public class MenuController {
 		stage.show();
 	}
 
+	////////////////////////////////////////////////////////////////////////////////////// profiles
+	@FXML
+	void addProfile(ActionEvent event) {
+		try {
+			// Check we don't have too many profiles already
+			if (profileButtons.getChildren().size() > 8) {
+				alert("Too many profiles!");
+				// Check there's at least something in the text box
+				// and the profiles doesn't already exist
+			} else if (!newProfileTextField.getText().equals("")
+					&& !ProfileFileReader.doesProfileExist(newProfileTextField.getText())) {
+
+				ProfileFileReader.createNewProfile(newProfileTextField.getText());
+				ProfileFileReader.loginProfile(newProfileTextField.getText());
+				
+				newProfileTextField.setText("");
+				profilesViewUpdated = false;
+				this.updateProfilesView();
+
+			}
+			// If the profile already exists
+			else if (!newProfileTextField.getText().equals("")) {
+				alert("Profile already exists");
+			}
+			// There's nothing in the text box
+			else {
+				alert("Please, type a name");
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+	@FXML
+	void removeProfile(ActionEvent event) {
+		try {
+
+			if (ProfileFileReader.getLoggedProfile() == null) {
+				alert("No profile is selected");
+			} else {
+				ProfileFileReader.deleteProfile(ProfileFileReader.getLoggedProfile());
+				ProfileFileReader.logout();;
+				HighScores.deleteProfile(ProfileFileReader.getLoggedProfile());
+				profilesViewUpdated = false;
+				this.updateProfilesView();
+			}
+
+		} catch (IOException ignored) {
+		}
+	}
+
+	public void updateProfilesView() {
+		if (!this.profilesViewUpdated) {
+			this.profilesViewUpdated = true;
+
+			String[] s = { "" };
+			try {
+				s = ProfileFileReader.getProfiles();
+			} catch (FileNotFoundException e) {
+				s[0] = "No profiles. Please Create a profile";
+			}
+			
+			profileButtons.getChildren().clear();
+			// Display a button for each profile
+			Button[] profButton = new Button[s.length];
+			
+			for (int i = 0; i < profButton.length; i++) {
+				profButton[i] = new Button(s[i]);
+				profButton[i].setPrefWidth(100);
+				profileButtons.getChildren().add(profButton[i]);
+
+				final int buttonIndex = i;
+				// Adds the action for each button
+				profButton[i].setOnAction(event -> {
+					ProfileFileReader.loginProfile(profButton[buttonIndex].getText());
+
+				});
+			}
+			
+			
+		}
+
+	}
+
+	////////////////////////////////////////////////////////////////////// levels
+
 	public void changeToMenu(ActionEvent event) throws IOException {
 		System.out.println("move to menu");
 
@@ -58,34 +188,5 @@ public class MenuController {
 		stage.setScene(scene);
 		stage.show();
 	}
-	
 
-	public void updateProfilesView() {
-		if (!this.profilesViewUpdated) {
-			this.profilesViewUpdated = true;
-			
-			String[] s = {""};
-	        try {
-	            s = ProfileFileReader.getProfiles();
-	        } catch (FileNotFoundException e) {
-	            s[0] = "No profiles. Please Create a profile";
-	        }
-
-	        // Display a button for each profile
-	        Button[] profButton = new Button[s.length];
-	        for (int i = 0; i < profButton.length; i++) {
-	            profButton[i] = new Button(s[i]);
-	            profButton[i].setPrefWidth(100);
-	            profileButtons.getChildren().add(profButton[i]);
-
-	            final int buttonIndex = i;
-	            // Adds the action for each button
-	            profButton[i].setOnAction(event -> {
-	                ProfileFileReader.loginProfile(profButton[buttonIndex].
-	                        getText());
-	            });
-	        }
-		} 
-
-	}
 }
