f5dacca809752b52b8d1a6e084e5a8fa2c323d2b
Mart1915339 <1915339@swansea.ac.uk>
2022-02-25 15:50:20 +0000

    Created TunnelB file and TunnelChild file

---
+++ src/main/java/TunnelChild.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * Class that defines a Tunnel.
+ *
+ * @author Alexander Douglas Lloyd-Ward
+ */
+public class TunnelChild extends Tile {
+    /**
+     * Tunnel constructor.
+     * Doesn't really do anything that the Tile doesn't do.
+     */
+    public TunnelChild() {
+        super(true, new ArrayList<>(), new ArrayList<>());
+    }
+}
